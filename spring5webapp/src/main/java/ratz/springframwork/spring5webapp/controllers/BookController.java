4c7bc11bd92414f31c8a482b064743b23a394a2d
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-08-29 19:03:38 +0000

    created and implemented Spring MVC with bookcontroler

---
+++ spring5webapp/src/main/java/ratz/springframwork/spring5webapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package ratz.springframwork.spring5webapp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ratz.springframwork.spring5webapp.repositories.BookRepository;
+
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
