7c3fe1212b2156772461a0be9fb20e8a7da1dcc3
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-08-29 19:25:09 +0000

    added thymeleaf, created books view and authors controller

---
+++ spring5webapp/src/main/java/ratz/springframwork/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package ratz.springframwork.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ratz.springframwork.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+    public AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors" , authorRepository.findAll());
+        return "authors";
+    }
+
+}
